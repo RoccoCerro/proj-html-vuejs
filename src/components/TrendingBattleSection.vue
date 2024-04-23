50b1e20dbcb69fb1fdacfe282c7c319b9e9fa321
RoccoCerro <rcerro02@gmail.com>
2024-04-23 19:33:20 +0200

    Add: style text-trending-battle

--- src/components/TrendingBattleSection.vue
+++ src/components/TrendingBattleSection.vue
@@ -4,11 +4,54 @@
       <!-- Text  -->
       <div class="text-trending-battle">
         <h3>Trending Battle</h3>
-        <h1>Keep Eyes & Manage Upcoming Battle</h1>
-        <p>liberolivero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cas ultricies ligula sed magna dictum porta.</p>
+        <h1>Keep Eyes & Manage <br> Upcoming Battle</h1>
+        <p>liberolivero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. <br> Cas ultricies ligula sed magna dictum porta.</p>
       </div>
       <!-- Row Card -->
-      
+      <div class="match">
+        <div class="row">
+          <div class="col-6">
+            <div class="card">
+              <div class="card-player">
+
+              </div>
+              <div class="match-date">
+
+              </div>
+              <div class="channels">
+
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="card">
+              <div class="card-player">
+
+              </div>
+              <div class="match-date"></div>
+              <div class="channels"></div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="card">
+              <div class="card-player">
+
+              </div>
+              <div class="match-date"></div>
+              <div class="channels"></div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="card">
+              <div class="card-player">
+
+              </div>
+              <div class="match-date"></div>
+              <div class="channels"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   </section>
