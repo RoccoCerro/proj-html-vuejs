486c6bb7bbb644929c0e7bdd854ef1411efbf780
RoccoCerro <rcerro02@gmail.com>
2024-04-23 19:14:27 +0200

    Add: title trendingBattle

--- src/components/TrendingBattleSection.vue
+++ src/components/TrendingBattleSection.vue
@@ -1,5 +1,16 @@
 <template>
   <section class="trending-battle">
+    <div class="container">
+      <!-- Text  -->
+      <div class="text-trending-battle">
+        <h3>Trending Battle</h3>
+        <h1>Keep Eyes & Manage Upcoming Battle</h1>
+        <p>liberolivero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cas ultricies ligula sed magna dictum porta.</p>
+      </div>
+      <!-- Row Card -->
+      
+    </div>
+
   </section>
 </template>
 
